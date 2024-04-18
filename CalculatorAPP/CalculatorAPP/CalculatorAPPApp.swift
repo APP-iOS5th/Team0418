351b44d780dda57b5d5941f7a251062d9500d5a8
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-18 13:30:47 +0900

    프로젝트 생성

---
+++ CalculatorAPP/CalculatorAPP/CalculatorAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAPPApp.swift
+//  CalculatorAPP
+//
+//  Created by 차지용 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
