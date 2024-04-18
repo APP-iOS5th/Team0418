5ed78cfe0d7f05460da1d84be0c4d20bc35008f0
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-18 16:02:14 +0900

    .빼고 기능 됨

--- CalculatorAPP/CalculatorAPP/ContentView.swift
+++ CalculatorAPP/CalculatorAPP/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     @State var result = 0
     @State var operators = ""
     
-    func CalculatorResult () -> Int{
+    func CalculatorResult() {
         switch operators{
         case "+":
             result = number1 + number2
@@ -26,12 +26,15 @@ struct ContentView: View {
         default :
             result = 0
         }
-        return result
+        result = result
     }
-
-    func rest () {
+    
+    
+    func rest() {
         number1 = 0
         number2 = 0
+        operators = ""
+        result = 0
     }
 
     
@@ -47,7 +50,12 @@ struct ContentView: View {
         VStack {
             HStack {
                 Button() {
-                 
+                    if operators.isEmpty {
+                        number1 = 7
+                    }
+                    else {
+                        number2 = 7
+                    }
                 }label: {
                     Text("7")
                         .padding(35)
@@ -56,7 +64,12 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        number1 = 8
+                    }
+                    else {
+                        number2 = 8
+                    }
                 }label: {
                     Text("8")
                         .padding(35)
@@ -65,7 +78,12 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        number1 = 9
+                    }
+                    else {
+                        number2 = 9
+                    }
                 }label: {
                     Text("9")
                         .padding(35)
@@ -74,7 +92,9 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        operators = "/"
+                    }
                 }label: {
                     Text("/")
                         .padding(35)
@@ -86,7 +106,12 @@ struct ContentView: View {
             }
             HStack {
                 Button() {
-                    
+                    if operators.isEmpty {
+                        number1 = 4
+                    }
+                    else {
+                        number2 = 4
+                    }
                 }label: {
                     Text("4")
                         .padding(35)
@@ -95,7 +120,12 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        number1 = 5
+                    }
+                    else {
+                        number2 = 5
+                    }
                 }label: {
                     Text("5")
                         .padding(35)
@@ -104,7 +134,12 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        number1 = 6
+                    }
+                    else {
+                        number2 = 6
+                    }
                 }label: {
                     Text("6")
                         .padding(35)
@@ -113,7 +148,9 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        operators = "*"
+                    }
                 }label: {
                     Text("*")
                         .padding(35)
@@ -125,7 +162,12 @@ struct ContentView: View {
             }
             HStack {
                 Button() {
-                    
+                    if operators.isEmpty {
+                        number1 = 1
+                    }
+                    else {
+                        number2 = 1
+                    }
                 }label: {
                     Text("1")
                         .padding(35)
@@ -134,7 +176,12 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        number1 = 2
+                    }
+                    else {
+                        number2 = 2
+                    }
                 }label: {
                     Text("2")
                         .padding(35)
@@ -143,7 +190,12 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        number1 = 3
+                    }
+                    else {
+                        number2 = 3
+                    }
                 }label: {
                     Text("3")
                         .padding(35)
@@ -152,7 +204,9 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        operators = "-"
+                    }
                 }label: {
                     Text("-")
                         .padding(35)
@@ -173,7 +227,12 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        number1 = 0
+                    }
+                    else {
+                        number2 = 0
+                    }
                 }label: {
                     Text("0")
                         .padding(35)
@@ -182,7 +241,7 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    rest()
                 }label: {
                     Text("C")
                         .foregroundColor(.red)
@@ -192,7 +251,9 @@ struct ContentView: View {
                         .border(Color.black)
                 }
                 Button() {
-                    
+                    if operators.isEmpty {
+                        operators = "+"
+                    }
                 }label: {
                     Text("+")
                         .padding(35)
@@ -202,7 +263,7 @@ struct ContentView: View {
                 }
             }
             Button() {
-                
+                CalculatorResult()
             }label: {
                 Text("=")
                     .padding(.horizontal,180)
