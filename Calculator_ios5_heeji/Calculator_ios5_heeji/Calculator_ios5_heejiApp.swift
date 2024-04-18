994906648ab03a0560361beb0887588151790397
Heeji Jung <heejijung@Heejiui-MacBookPro.local>
2024-04-18 14:18:27 +0900

    Create new projects and design calculators

---
+++ Calculator_ios5_heeji/Calculator_ios5_heeji/Calculator_ios5_heejiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_ios5_heejiApp.swift
+//  Calculator_ios5_heeji
+//
+//  Created by Heeji Jung on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_ios5_heejiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator_ios5_heeji/Calculator_ios5_heeji/ContentView.swift
@@ -0,0 +1,222 @@
+//
+//  ContentView.swift
+//  Calculator_ios5_heeji
+//
+//  Created by Heeji Jung on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.black.ignoresSafeArea()
+            VStack {
+                /*계산 결과값*/
+                HStack{
+                    Spacer()
+                    Text("123123")
+                        .padding()
+                        .font(.system(size: 70))
+                        .foregroundColor(.white)
+                }
+                /*자판*/
+                HStack{
+                    Button{
+                    }label: {
+                        Text("AC")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("+/-")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("%")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("÷")
+                            .frame(width:80, height: 80)
+                            .background(.orange)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 50))
+                    }
+                }
+                HStack{
+                    Button{
+                    }label: {
+                        Text("7")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("8")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("9")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("X")
+                            .frame(width:80, height: 80)
+                            .background(.orange)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                }
+                HStack{
+                    Button{
+                    }label: {
+                        Text("4")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("5")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("6")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("-")
+                            .frame(width:80, height: 80)
+                            .background(.orange)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                }
+                HStack{
+                    Button{
+                    }label: {
+                        Text("1")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("2")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("3")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("+")
+                            .frame(width:80, height: 80)
+                            .background(.orange)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                }
+                HStack{
+                    Button{
+                    }label: {
+                        Text("0")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text(".")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("6")
+                            .frame(width:80, height: 80)
+                            .background(.gray)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                    Button{
+                    }label: {
+                        Text("=")
+                            .frame(width:80, height: 80)
+                            .background(.orange)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .font(.system(size: 33))
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
