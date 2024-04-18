aca0f32000294418b2d365c938278af99f3371a6
jinwoong <baseball0020@gmail.com>
2024-04-18 14:50:23 +0900

    feat: configure `ContentView` with calculation view and logic

---
+++ SwiftUICalculator/SwiftUICalculator/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  SwiftUICalculator
+//
+//  Created by jinwoong Kim on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var equation = "0"
+    
+    private let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
+    private let items: [CalculatorElement] = [
+        .operand(7), .operand(8), .operand(9), .operator("/"),
+        .operand(4), .operand(5), .operand(6), .operator("*"),
+        .operand(1), .operand(2), .operand(3), .operator("-"),
+        .floatingPoint, .operand(0), .clear, .operator("+")
+    ]
+    
+    var body: some View {
+        VStack(alignment: .trailing) {
+            Text(equation)
+                .font(.largeTitle)
+                .padding()
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(items) { item in
+                    Button {
+                        appendElement(with: item)
+                    } label: {
+                        Text(item.displayElement)
+                            .foregroundStyle(item.isClearElement ? .red : .black)
+                    }
+
+                }
+            }
+            .buttonStyle(.calculate)
+            Button("=") {
+                calculate()
+            }
+            .buttonStyle(.calculate)
+        }
+    }
+    
+    func appendElement(with element: CalculatorElement) {
+        if element.isClearElement {
+            equation = ""
+        } else {
+            equation.append(element.displayElement)
+        }
+    }
+    
+    func calculate() {
+        let expression = NSExpression(format: equation)
+        if let result = expression.expressionValue(with: nil, context: nil) as? Int {
+            equation = "\(result)"
+        } else {
+            equation = ""
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
