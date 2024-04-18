b25aad7ef020229395d6f508fcde0b6095e82334
jinwoong <baseball0020@gmail.com>
2024-04-18 14:15:17 +0900

    chore: add initialized project

---
+++ SwiftUICalculator/SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by jinwoong Kim on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
