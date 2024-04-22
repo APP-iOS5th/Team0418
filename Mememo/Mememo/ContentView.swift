f5a3920bd0c8950d4cc74acb4b6589f86f571b81
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-22 15:35:48 +0900

    ContentView 구현

--- Mememo/Mememo/ContentView.swift
+++ Mememo/Mememo/ContentView.swift
@@ -45,11 +45,56 @@ class MemoStroe: ObservableObject {
 }
 
 struct ContentView: View {
+    @ObservedObject var memoStore: MemoStroe = MemoStroe()
+    
+    @State var isSheetShowing: Bool = false
+    @State var memoText: String = ""
+    @State var memoColor: Color = .blue
+    let colors:[Color] = [.blue, .cyan, .purple, .yellow, .indigo]
+    
     var body: some View {
-
+        NavigationStack {
+            List(memoStore.memos) { memo in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(memo.text)").font(.title)
+                        Text("\(memo.createdString)").font(.body).padding(.top)
+                    }
+                    Spacer()
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(memo.color)
+                .shadow(radius: 3)
+                .padding()
+                .contextMenu {
+                    ShareLink(item: memo.text)
+                    Button {
+                        memoStore.removeMemo(memo)
+                    } label: {
+                        Image(systemName: "trash.slash")
+                        Text("삭제")
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("mememo")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("추가") {
+                        memoText = ""
+                        isSheetShowing =  true
+                    }
+                }
+            }
+            .sheet(isPresented: $isSheetShowing) {
+                MemoAddView()
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
