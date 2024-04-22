b534e398da8c2b889c16279cd1ef3bf1190b1485
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-22 15:35:14 +0900

    메모 모델 및 저장소 구현

--- Mememo/Mememo/ContentView.swift
+++ Mememo/Mememo/ContentView.swift
@@ -6,6 +6,43 @@
 //
 
 import SwiftUI
+import Foundation
+import SwiftData
+
+@Model
+struct Memo {
+    var id: UUID = UUID()
+    var text: String
+    var color: Color
+    var created: Date
+    
+    init(text: String, color: Color, created: Date) {
+        self.text = text
+        self.color = color
+        self.created = created
+    }
+    var createdString: String {
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: created)
+    }
+}
+
+class MemoStroe: ObservableObject {
+    @Published var memos: [Memo] = []
+    
+    //추가
+    func addMemo(_ text: String, color: Color) {
+        let memo: Memo = Memo(text: text, color: color, created: Date())
+    }
+    
+    //삭제
+    func removeMemo(_ targetMemo: Memo) {
+        if let index = memos.firstIndex(where: {$0.id == targetMemo.id}) {
+            memos.remove(at: index)
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
